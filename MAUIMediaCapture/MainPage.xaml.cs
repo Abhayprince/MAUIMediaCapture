b4afc38b42aab6f2ac51ddd6b96dfe8a57ffc0ee
Abhay Prince <46496449+Abhayprince@users.noreply.github.com>
2025-03-20 19:15:37 +0530

    Initial

---
+++ MAUIMediaCapture/MainPage.xaml.cs
@@ -0,0 +1,101 @@
+﻿namespace MAUIMediaCapture
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void CapturePhotoBtn_Clicked(object sender, EventArgs e)
+        {
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                FileResult? fileResult = await MediaPicker.Default.CapturePhotoAsync();
+                if (fileResult is null)
+                {
+                    // User canceled
+                    await DisplayAlert("Alert", "No photo captured", "Ok");
+                    return;
+                }
+                var photoStream = await fileResult.OpenReadAsync();
+
+                // You can use this photoStream to perform different options
+                // 1. Show it on the UI
+                // 2. Save it in the SQLIte database
+                // 3. Send this photo to API
+                // 4. Resize/crop or edit operation on this image
+
+                ShowPhotoStreamOnUI(photoStream);
+                return;
+
+                // save this image on device
+                await SavePhotoToDeviceAsync(photoStream, fileResult.FileName);
+                return;
+
+                await SavePhotoStreamToLocalDbAsync(photoStream);
+                return;
+
+                await SendPhotoStreamToAPIAsync(photoStream);
+                return;
+
+                await SavePhotoStreamToLocalDbAsync(photoStream);
+                return;
+
+                ResizePhoto(photoStream);
+                return;
+
+                AdvancedImageEditing(photoStream);
+            }
+            else
+            {
+                // show some alert or toast message
+            }
+        }
+
+        private void ShowPhotoStreamOnUI(Stream photoStream)
+        {
+            Img.Source = ImageSource.FromStream(() => photoStream);
+        }
+        private async Task SavePhotoToDeviceAsync(Stream photoStream, string fileName)
+        {
+            var path = Path.Combine(FileSystem.CacheDirectory, fileName);
+            using var fs = File.Create(path);
+            await photoStream.CopyToAsync(fs);
+        }
+
+        private async Task SendPhotoStreamToAPIAsync(Stream photoStream)
+        {
+            // REFIT - //StreamPart = photoStream;
+            // HTTPClient
+            //StreamContent 
+            //ByteArrayContent
+            //MultipartFormDataContent
+            var ms = new MemoryStream();
+            photoStream.CopyTo(ms);            
+            var byteArray = ms.ToArray();
+        }
+
+        private async Task SavePhotoStreamToLocalDbAsync(Stream photoStream)
+        {
+            // in SQLite we can have byte[] column to save the binary data
+            var ms = new MemoryStream();
+            photoStream.CopyTo(ms);
+            var byteArray = ms.ToArray();
+
+            // Save this byteArray to the database
+        }
+
+        private void ResizePhoto(Stream photoStream)
+        {
+            // BAsic resizing we can do with GraphicsView in .NET MAUI
+        }
+
+        private void AdvancedImageEditing(Stream photoStream)
+        {
+            // Use some thrid party compnnet/control
+            // for example Syncfusion ImageEditor control
+        }
+    }
+
+}
